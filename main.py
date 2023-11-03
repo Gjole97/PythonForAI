185434d8c41fed2f68243770cc162603265402ab
Gjole97 <96892119+Gjole97@users.noreply.github.com>
2023-11-03 18:26:24 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+pd.set_option('display.max_columns', 500)
+data = pd.read_csv('excel-comp-data.csv')
+
+for index, row in data.iterrows():
+    print('Name:', row['name'], 'City:', row['city'], 'Income:', row['Jan']+row['Feb']+row["Mar"])
